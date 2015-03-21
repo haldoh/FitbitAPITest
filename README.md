bbb2938dc10c39b7099031499a1f3edd5726ca9a
Aldo Ambrosioni <aldo.ambrosioni@gmail.com>
2015-03-21 13:00:04 +0100

    Working auth

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ FitbitAPITest
 =============
 
 A testbed for Fitbit API / MEAN stack integration
+
+Thanks to [Jared Hanson](http://jaredhanson.net/) for the passport-fitbit library and the useful example included.
--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var passport = require('passport');
+var util = require('util');
+var FitbitStrategy = require('passport-fitbit').Strategy;
+
+var FITBIT_CONSUMER_KEY = "c2e4152b68af4d4b9ba41134275cdff2"
+var FITBIT_CONSUMER_SECRET = "ff3cc073d9ce47c38b73f9c1fda41bf8";
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +29,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+	secret: "Power of the dragonflame",
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+	done(null, obj);
+});
+
+// Use the FitbitStrategy within Passport.
+// Strategies in passport require a `verify` function, which accept
+// credentials (in this case, a token, tokenSecret, and Fitbit profile), and
+// invoke a callback with a user object.
+passport.use(new FitbitStrategy({
+		consumerKey: FITBIT_CONSUMER_KEY,
+		consumerSecret: FITBIT_CONSUMER_SECRET,
+		callbackURL: "http://192.168.0.9:3000/auth/fitbit/callback"
+	},
+		function(token, tokenSecret, profile, done) {
+		// asynchronous verification, for effect...
+		process.nextTick(function () {
+			// To keep the example simple, the user's Fitbit profile is returned to
+			// represent the logged-in user. In a typical application, you would want
+			// to associate the Fitbit account with a user record in your database,
+			// and return that user instead.
+			return done(null, profile);
+		});
+	}
+));
 
 app.use('/', routes);
 app.use('/users', users);
